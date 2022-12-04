4574d3a06c39a77393b69154cfcf6a5af8608b36
Ezechiel <sxedesign@gmail.com>
2022-12-04 23:14:05 +0100

    just before world end.

--- ClientApp/src/app/projects/job-selector/job-selector.component.ts
+++ ClientApp/src/app/projects/job-selector/job-selector.component.ts
@@ -2,11 +2,30 @@ import {
   AfterViewInit,
   Component,
   ElementRef,
+  EventEmitter,
   Input,
+  OnInit,
   Output,
   ViewChild,
 } from '@angular/core';
-import { BehaviorSubject, fromEvent, Observable, tap } from 'rxjs';
+import { EventType } from '@angular/router';
+import {
+  BehaviorSubject,
+  concatMap,
+  debounce,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  fromEvent,
+  map,
+  mergeMap,
+  Observable,
+  of,
+  pipe,
+  startWith,
+  switchMap,
+  tap,
+} from 'rxjs';
 import { Category, Job } from 'src/app/_models/job.model';
 
 @Component({
@@ -14,26 +33,47 @@ import { Category, Job } from 'src/app/_models/job.model';
   templateUrl: './job-selector.component.html',
   styleUrls: ['./job-selector.component.css'],
 })
-export class JobSelectorComponent implements AfterViewInit {
-  @Input() categories: Observable<Category[]>;
-  @Input() jobs: Observable<Job[]>;
-  jobList$: BehaviorSubject<Job[]> = new BehaviorSubject<Job[]>([]);
-  @ViewChild('categoryInput') catInputRef: ElementRef;
-
-  /**
-   *
-   */
-  constructor() {}
-  ngAfterViewInit(): void {
-    fromEvent(this.catInputRef.nativeElement, 'change')
-      .pipe(tap((e: any) => console.log('xD', e.target.value)))
-      .subscribe();
+export class JobSelectorComponent implements OnInit {
+  @Input() categories: Category[];
+  // jobs list filtered by category at smart component
+  @Input() jobs: Job[];
+  @Output() selectedCategory = new EventEmitter<string>();
+  //Search box
+  @ViewChild('jobSearchBox') jobSearchBoxRef: ElementRef;
+  jobs$: Observable<Job[]>;
+  searchInput$: Observable<string>;
+  constructor(private el: ElementRef) {}
+  ngOnInit(): void {
+    this.searchInput$ = fromEvent<InputEvent>(
+      this.el.nativeElement,
+      'keyup'
+    ).pipe(
+      map((event) => (event.target as HTMLInputElement).value),
+      filter((search_val: string) => search_val.length > 2),
+      distinctUntilChanged(),
+      debounceTime(300)
+    );
+    console.log('CHUJ', this.jobs);
+    this.jobs$ = of(this.jobs).pipe(
+      concatMap(
+        () => this.searchInput$,
+        (ja: Job[], input: string) => {
+          console.log('jobs ', ja);
+          console.log('input: ', input);
+          return ja.filter((j) => j.name.indexOf(input) != -1);
+        }
+      ),
+      tap((x: any) => console.log('asd', x))
+    );
+    this.jobs$.subscribe();
   }
+  ngAfterViewInit(): void {}
 
   // hElement: HTMLInputElement = this.elRef.NativeElement;
   onCategoryChange(selectedValue: string) {
     console.log('selected category:', selectedValue);
-
+    this.selectedCategory.emit(selectedValue);
+    console.log('CHUJ', this.jobs);
     // fromEvent(categoryInput)
   }
 }
--- ClientApp/src/app/projects/projects.component.ts
+++ ClientApp/src/app/projects/projects.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiClientService } from '@services/api-client.service';
 import { JobsService } from '@services/jobs.service';
-import { Observable } from 'rxjs';
-import { Category } from '../_models/job.model';
+import {
+  BehaviorSubject,
+  distinct,
+  Observable,
+  switchMap,
+  map,
+  mergeMap,
+} from 'rxjs';
+import { Category, Job } from '../_models/job.model';
 
 @Component({
   selector: 'app-projects',
@@ -12,11 +19,28 @@ import { Category } from '../_models/job.model';
 export class ProjectsComponent implements OnInit {
   response: any;
   categories$: Observable<Category[]>;
+  jobs$: BehaviorSubject<Job[]> = new BehaviorSubject<Job[]>([] as Job[]);
+  selectedCategoryName$ = new BehaviorSubject<string>('Any');
   constructor(private jobsService: JobsService) {
-    this.categories$ = jobsService.categories$;
+    this.categories$ = this.jobsService.categories$;
   }
 
   ngOnInit(): void {
     this.jobsService.refreshJobs().subscribe();
+    this.selectedCategoryName$
+      .pipe(
+        switchMap((cat_name: string) =>
+          this.jobsService.jobs$.pipe(
+            map((j: Job[]) => {
+              if (cat_name === 'Any') return j;
+              else return j.filter((i) => i.category.name === cat_name);
+            })
+          )
+        )
+      )
+      .subscribe(this.jobs$);
+  }
+  filterJobsByCategory(categoryName: string) {
+    this.selectedCategoryName$.next(categoryName);
   }
 }
