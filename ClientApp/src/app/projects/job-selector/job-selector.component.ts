fddb6e8db4566c5ec3a66fb2f139a38c15a014d0
Ezechiel <sxedesign@gmail.com>
2022-12-06 21:56:22 +0100

    filtering on searchbox working todo: click action

--- ClientApp/src/app/projects/job-selector/job-selector.component.ts
+++ ClientApp/src/app/projects/job-selector/job-selector.component.ts
@@ -19,28 +19,46 @@ import {
   fromEvent,
   map,
   mergeMap,
+  mergeMapTo,
   Observable,
   of,
   pipe,
   startWith,
   switchMap,
   tap,
+  withLatestFrom,
 } from 'rxjs';
 import { Category, Job } from 'src/app/_models/job.model';
 
 @Component({
   selector: 'app-job-selector',
-  templateUrl: './job-selector.component.html',
+  // templateUrl: './job-selector.component.html',
+  template: `<select
+      (change)="onCategoryChange(categoryInput.value)"
+      #categoryInput
+      class="outline-none"
+    >
+      <option>Any</option>
+      <option *ngFor="let category of categories">
+        {{ category.name }}
+      </option>
+    </select>
+    <div><input #jobSearchBox type="text" name="" id="" /></div>
+    <div>
+      <button class="p-2" *ngFor="let job of filteredJobs$ | async">
+        {{ job.name }}
+      </button>
+    </div> `,
   styleUrls: ['./job-selector.component.css'],
 })
 export class JobSelectorComponent implements OnInit {
   @Input() categories: Category[];
   // jobs list filtered by category at smart component
-  @Input() jobs: Job[];
+  @Input() jobs$: Observable<Job[]>;
   @Output() selectedCategory = new EventEmitter<string>();
   //Search box
   @ViewChild('jobSearchBox') jobSearchBoxRef: ElementRef;
-  jobs$: Observable<Job[]>;
+  filteredJobs$: Observable<Job[]>;
   searchInput$: Observable<string>;
   constructor(private el: ElementRef) {}
   ngOnInit(): void {
@@ -51,21 +69,20 @@ export class JobSelectorComponent implements OnInit {
       map((event) => (event.target as HTMLInputElement).value),
       filter((search_val: string) => search_val.length > 2),
       distinctUntilChanged(),
-      debounceTime(300)
+      debounceTime(300),
+      startWith('')
     );
-    console.log('CHUJ', this.jobs);
-    this.jobs$ = of(this.jobs).pipe(
-      concatMap(
-        () => this.searchInput$,
-        (ja: Job[], input: string) => {
-          console.log('jobs ', ja);
-          console.log('input: ', input);
-          return ja.filter((j) => j.name.indexOf(input) != -1);
+    this.filteredJobs$ = this.jobs$.pipe(
+      withLatestFrom(this.searchInput$),
+      map(([jobs, input]) => {
+        if (input.length > 0) {
+          return jobs.filter((j) => j.name.indexOf(input) != -1);
+        } else {
+          return jobs;
         }
-      ),
-      tap((x: any) => console.log('asd', x))
+      })
     );
-    this.jobs$.subscribe();
+    // this.jobs$.subscribe();
   }
   ngAfterViewInit(): void {}
 
@@ -73,7 +90,5 @@ export class JobSelectorComponent implements OnInit {
   onCategoryChange(selectedValue: string) {
     console.log('selected category:', selectedValue);
     this.selectedCategory.emit(selectedValue);
-    console.log('CHUJ', this.jobs);
-    // fromEvent(categoryInput)
   }
 }
